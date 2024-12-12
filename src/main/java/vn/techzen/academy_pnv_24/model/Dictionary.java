87b4f050da8695487a616778e0dda29882c36f6f
Mai74D <nguyenmaioc0@gmail.com>
2024-12-12 11:38:52 +0700

    Search feature (#1)
    Co-authored-by: Mai74D <mai.ho25@student.passerellesnumeriques.org>

---
+++ src/main/java/vn/techzen/academy_pnv_24/model/Dictionary.java
@@ -0,0 +1,39 @@
+package vn.techzen.academy_pnv_24.model;
+
+public class Dictionary {
+    private int id;
+    private String english_word;
+    private String translation;
+
+
+    public Dictionary() {}
+    public Dictionary(int id, String english_word,String translation) {
+        this.id = id;
+        this.translation = translation;
+        this.english_word = english_word;
+    }
+
+    public String getEnglish_word() {
+        return english_word;
+    }
+
+    public void setEnglish_word(String english_word) {
+        this.english_word = english_word;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
